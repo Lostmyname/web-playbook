5714cdb8d2ead52a3e4ddb6572f3b9c9010ea3c5
Daniel Grant <danieljohngrant@gmail.com>
2017-01-25 17:04:18 +0000

    Restructure to use common folder for components

--- packages/react-scripts/template/src/client/index.js
+++ packages/react-scripts/template/src/client/index.js
@@ -1,7 +1,6 @@
-import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../common/components/App';
 
 ReactDOM.render(
   <App />,
--- packages/react-scripts/template/src/common/components/App/App.js
+++ packages/react-scripts/template/src/common/components/App/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App = React.createClass({
@@ -7,15 +6,10 @@ const App = React.createClass({
     return (
       <div className="App">
         <div className="App-header">
-          <img
-            src={logo}
-            className="App-logo"
-            alt="logo"
-          />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/common/App.js</code> and save to reload.
         </p>
       </div>
     );
