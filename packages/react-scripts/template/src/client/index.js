7a35e4d3d9a378981bdd2b426fd083aacdc9364e
Daniel Grant <danieljohngrant@gmail.com>
2017-01-26 16:54:30 +0000

    Add react-router client side

--- packages/react-scripts/template/src/client/index.js
+++ packages/react-scripts/template/src/client/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import App from '../universal/components/App';
+import { Router, browserHistory } from 'react-router';
 import reducers from '../universal/store/reducers';
 import storeEnhancer from '../universal/store/storeEnhancer';
+import routes from '../universal/routes';
 
 var initialState = {};
 var store = createStore(reducers, initialState, storeEnhancer);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router
+      routes={routes}
+      history={browserHistory}
+    />
   </Provider>,
   document.getElementById('root')
 );
